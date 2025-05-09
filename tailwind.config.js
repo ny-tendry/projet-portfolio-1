ddeb99570c3755176c4131ade1d0e5308408a5de
ny-tendry <nytendry2@gmail.com>
2025-05-09 15:57:38 +0300

    premier commit: version initiale

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        '3xl': '0 0 60px rgba(0, 0, 0, 0.3)',
+        '4xl': '0 0 80px rgba(0, 0, 0, 0.25)',
+        '5xl': '0 0 100px rgba(0, 0, 0, 0.8)',
+        'giant': '0 0 150px rgba(0, 0, 0, 400)'
+      },
+      width: {
+        'longueur': '90%',
+      },
+      gap:{
+        'distance':'400px'
+      },
+      
+      
+    },
+  },
+  plugins: [],
+}
+
